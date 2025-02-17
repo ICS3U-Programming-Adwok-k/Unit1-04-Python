8d3a0498db83e63061a2326cf99b25f22586224b
Adwok Adiebo <adwok.adiebo@stu.ocsb.ca>
2025-02-17 22:39:24 +0000

    basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Adwok Adiebo
+
+# Date: Feb 17, 2025
+
+# This program does basic math.
+
+
+def main():
+    print("5+6={}".format(5 + 6))
+    print()
+
+
+if __name__ == "__main__":
+    main()
