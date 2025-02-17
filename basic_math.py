53adebbf4fa169fee0b9e4de59b9099085556a12
Adwok-k <adwok.adiebo@stu.ocsb.ca>
2025-02-17 18:52:45 -0500

    Update basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -1,1 +1,22 @@
+Unit1-04-Python
+#!/usr/bin/env python3
 
+# Created By: Adwok Adiebo
+
+# Date: Feb 17, 2025
+
+# This program does basic math.
+
+
+def main():
+    print("5+6={}".format(5 + 6))
+    print("13-6={}".format(13-6))
+    print("5*8={}".format(5*8))
+    print("9/3={}".format(9/3))
+    print("4**2={}".format(4**2))
+    print("100**0.5={}".format(100**0.5))
+    
+
+
+if __name__ == "__main__":
+    main()
