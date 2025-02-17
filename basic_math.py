22ff16a2ea7dfa745616e59be825b2200e695c97
Adwok-k <adwok.adiebo@stu.ocsb.ca>
2025-02-17 17:45:30 -0500

    Update basic_math.py

--- basic_math.py
+++ basic_math.py
@@ -8,8 +8,12 @@
 
 
 def main():
-    print("5+6={}".format(5 + 6))
-    print()
+    print("5+6={}".format(5+6))
+    print("4*5={}".format(4*5))
+    print("10-9={}".format(10-9))
+    print("18/9={}".format(18/9))
+    print("10**2={}".format(10**2))
+    print("16√4={}".format(1√4))
 
 
 if __name__ == "__main__":
